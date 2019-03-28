42f2d8e426e1f016b171bf7be92619e3e1ec6398
Marco Berger <marco@fiddich.com>
2019-03-28 08:50:36 +0100

    fix url

--- misc/install_docker-compose.sh
+++ misc/install_docker-compose.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -L "https://github.com/docker/compose/release/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
